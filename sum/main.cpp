202d4ce5fbea46f1af751bee33077ff07eeba857
weversonbarbieri <weverson.barbieri@gmail.com>
2024-02-18 16:32:18 -0400

    Sum - C++
    Enter x and Y value, then the algorithm sums those 2 numbers and prints the result.

---
+++ sum/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int x, y, sum;
+
+    cout << "Enter X value: ";
+    cin >> x;
+    cout << "Enter Y value: ";
+    cin >> y;
+
+    sum = x + y;
+
+    cout << "Sum of X and Y is = " << sum << endl;
+
+    return 0;
+}
